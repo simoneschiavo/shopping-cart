6a2ebf497d7d668de94bd601e57a19278a2c54a6
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-27 10:48:13 +0100

    feat(Header): add styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import logoIpsum from "../assets/Logoipsum.svg";
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center p-6 text-xs">
       <img src={logoIpsum} alt="logo" />
-      <nav className="navbar">
-        <ul>
-          <li className="font-bold">Home</li>
+      <nav>
+        <ul className="flex gap-4">
+          <li>Home</li>
           <li>Cart</li>
         </ul>
       </nav>
