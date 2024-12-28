1f6564cce4cef39ac851d6e1c38ce645c76a3fd8
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-28 15:11:15 +0100

    feat(DefaulProducts): add main logic

--- src/pages/Products/DefaultProducts.jsx
+++ src/pages/Products/DefaultProducts.jsx
@@ -1,5 +1,31 @@
 import { useState, useEffect } from "react";
 
 export default function DefaultProducts() {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const data = await getRequestsWithNativeFetch(
+          "https://fakestoreapi.com/products"
+        );
+        setProducts(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {products && products.map((product) => <div key={product.id}><ProductCard product={product} /></div>)}
+    </div>
+  );
 }
