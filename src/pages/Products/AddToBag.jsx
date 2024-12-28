cdf2e92e5d5af64acbd4ecc6d928597c71f6e901
Simone Schiavo <simoneschiavo@icloud.com>
2024-12-28 17:33:56 +0100

    feat(AddToBag): add default value of 1

--- src/pages/Products/AddToBag.jsx
+++ src/pages/Products/AddToBag.jsx
@@ -1,12 +1,12 @@
 export default function AddToBag({ product, handleAddToCart }) {
-    const handleSubmit = () => {
+  const handleSubmit = () => {
     const quantity = document.getElementById("quantity").value;
     const productWithQuantity = {
-        ...product,
-        quantity: quantity
-    }
+      ...product,
+      quantity: quantity,
+    };
     handleAddToCart(productWithQuantity);
-}
+  };
 
   return (
     <div className="mt-auto flex justify-between gap-4 text-sm">
@@ -15,9 +15,13 @@ export default function AddToBag({ product, handleAddToCart }) {
         name="quantity"
         id="quantity"
         min={1}
+        defaultValue={1}
         className="w-16 border border-gray-500 px-2 py-1"
       />
-      <button className="basis-3/4 bg-blue-700 text-white px-6 py-2 uppercase" onClick={handleSubmit}>
+      <button
+        className="basis-3/4 bg-blue-700 text-white px-6 py-2 uppercase"
+        onClick={handleSubmit}
+      >
         Add to bag
       </button>
     </div>
